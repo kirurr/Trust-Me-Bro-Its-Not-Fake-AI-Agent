ec0f9be2d45d12ad4efc08ae2f0a18f14264867f
kirurr <timon09df@gmail.com>
2026-04-14 17:38:53 +0300

    add: ws reconnection on the client

--- web/src/chat/ws.ts
+++ web/src/chat/ws.ts
@@ -1,13 +1,49 @@
 let ws: WebSocket | null = null;
+let reconnectTimeout: ReturnType<typeof setTimeout> | null = null;
+
+const WS_URL = import.meta.env.VITE_BACKEND_URL || "ws://localhost:8080/ws";
+const RECONNECT_DELAY = 2000;
+
+function connect() {
+  ws = new WebSocket(WS_URL);
+
+  ws.addEventListener("open", () => {
+    console.log("WebSocket connection established");
+
+    if (reconnectTimeout) {
+      clearTimeout(reconnectTimeout);
+      reconnectTimeout = null;
+    }
+  });
+
+  ws.addEventListener("close", () => {
+    console.log("WebSocket connection closed");
+
+    reconnectTimeout = setTimeout(() => {
+      connect();
+    }, RECONNECT_DELAY);
+  });
+
+  ws.addEventListener("error", (e) => {
+    console.error("WebSocket error: ", e);
+
+    ws?.close();
+  });
+}
 
 export const getWs = () => {
-	if (!ws || ws.readyState === WebSocket.CLOSED) {
-		ws = new WebSocket("ws://localhost:8080/ws");
-	}
-	return ws;
-}
+  if (!ws || ws.readyState === WebSocket.CLOSED) {
+    connect();
+  }
+  return ws;
+};
 
 export const closeWs = () => {
-	ws?.close();
-	ws = null;
-}
+  if (reconnectTimeout) {
+    clearTimeout(reconnectTimeout);
+    reconnectTimeout = null;
+  }
+
+  ws?.close();
+  ws = null;
+};
